266543be1175eee44da10567d67cf7a3fae209ae
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-01-31 10:03:49 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Dhanush Tipparaju on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
